f0cb9157b821f87c1f1ca57470b836fad163b0a0
Albert <albertnt905@gmail.com>
2022-06-15 02:23:06 -0400

    -

---
+++ LearningNode/Intro/async-demo.js
@@ -0,0 +1,11 @@
+//fs is a method that reads files
+fs = require('fs');
+
+function phoneNumber(err, data){
+    console.log('data:', data);
+}
+
+//Instead of the execution waiting for readdir to finish, it goes onto the next line, then when readdir finishes it calls the function (callback) to execute the console.log
+fs.readdir('c:/', phoneNumber);
+
+console.log("this comes after");
