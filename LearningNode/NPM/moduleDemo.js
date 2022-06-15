8fa2e9f48f5e281688958b20e2185550c30c0448
albert <albertnt905@gmail.com>
2022-06-15 14:22:56 -0400

    -

--- LearningNode/NPM/moduleDemo.js
+++ LearningNode/NPM/moduleDemo.js
@@ -1,3 +1,4 @@
 var myModule = require('./myModule.js')
 
 console.log(myModule.myText)
+console.log(myModule.myCounter())
--- LearningNode/NPM/myModule.js
+++ LearningNode/NPM/myModule.js
@@ -1,1 +1,10 @@
 exports.myText = "hello from module"
+exports.myCounter = () => {
+    count = 0
+
+    for(var i=0; i<10; i++){
+        count = count + i
+    }
+
+    return count
+}
