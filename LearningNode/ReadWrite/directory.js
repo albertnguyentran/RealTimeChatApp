c2302ae9d470e5b211cd22ea958f003d560024c2
albert <albertnt905@gmail.com>
2022-06-20 23:57:13 -0400

    -

--- LearningNode/ReadWrite/directory.js
+++ LearningNode/ReadWrite/directory.js
@@ -1,6 +1,7 @@
 fs = require('fs')
 
 //Callback function executes once readdir finishes, however it is not synchronous, it is async
+//The err is a paremeter passed automatically into the function by the method, if an err does occur, it will be stored as the var err
 fs.readdir('/System/Library', (err, data) => {
     console.log(data)
 })
