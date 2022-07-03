7d7cdbf9f94ef596827075a5c0a88654a6eed148
albert <albertnt905@gmail.com>
2022-07-03 01:56:06 -0400

    -

--- WebApp/server.js
+++ WebApp/server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
-//Makes it possible to acces files from the __dirname file
+//Makes it possible to access files from the __dirname file
 app.use(express.static(__dirname))
 //app.use sets up the bodyParser as middleware
 //the following .json() tells the bodyParser method to expect JSON to be coming in with the HTTP request
@@ -16,13 +16,17 @@ var messages = [
 ]
 
 //Creates an endpoint at route /messages
+//GET simply means retrieve data from a specified resource
+//Here we are routing all get requests to the specified path with the specified callback functions 
+//Response is to send the var messages to the endpoint
 app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
-
+//POST simply means submit data to be processed to a specificed resource
+//Here we are routing all post requests to the specified path with the specified callback functions 
+//Whenever a post request is made, the data is pushed into the messages array and the response is to send a status update to confirm its success
 app.post('/messages', (req, res) => {
-    //Messages.push appends the req.body (request) into the messages array, which is then sent to the /messages endpoint and obtained and added with the function addMessages 
     messages.push(req.body)
     res.sendStatus(200)
 })
